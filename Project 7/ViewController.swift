83864b6ad65ce15e59ad83e33bfc724d98d769bf
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-12-01 09:09:09 +0100

    Add TableView Configurations

--- Project 7/ViewController.swift
+++ Project 7/ViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var petetions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petetions.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Title goes here"
+        content.secondaryText = "Subtitle goes here"
+        cell.contentConfiguration = content
+        return cell
     }
 }
