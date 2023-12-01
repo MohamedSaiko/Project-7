caf7d63457b913594a5c7a1bb67ee87a104a062a
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-12-01 08:45:43 +0100

    Add TableViewController, NavigationController and TabBarController

---
+++ Project 7/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Project 7
+//
+//  Created by Mohamed Sayed on 01.12.23.
+//
+
+import Foundation
+
+let cellIdentifier = "Cell"
+let navigationControllerID = "NavController"
